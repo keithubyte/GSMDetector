a0294318f715b6e4559b95d4d370f839262b919b
Keith Leong <justgleliang@gmail.com>
2017-07-31 11:23:59 +0800

    增加定时器来刷新数据

--- app/src/main/java/com/bluedon/gsm/detector/GSMDataFragment.java
+++ app/src/main/java/com/bluedon/gsm/detector/GSMDataFragment.java
@@ -5,11 +5,18 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindString;
 import butterknife.BindView;
@@ -36,7 +43,49 @@ public class GSMDataFragment extends Fragment {
     @BindString(R.string.model)
     String m;
 
-    private GSMData mGSMData;
+    private GSMCellInfoAdapter adapter;
+    private GSMData data;
+    private ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private ScheduledFuture futures;
+    private Runnable refresh = new Runnable() {
+        @Override
+        public void run() {
+            Context context = getActivity();
+            data = new GSMData(context);
+            handler.obtainMessage(MSG_UPDATE_DATA).sendToTarget();
+        }
+    };
+
+    private MHandler handler;
+    private static final int MSG_UPDATE_DATA = 0x100;
+
+    private static class MHandler extends Handler {
+        private WeakReference<GSMDataFragment> mFragmentRef;
+
+        MHandler(GSMDataFragment fragment) {
+            mFragmentRef = new WeakReference<>(fragment);
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void handleMessage(Message msg) {
+            GSMDataFragment f = mFragmentRef.get();
+            if (f != null) {
+                switch (msg.what) {
+                    case MSG_UPDATE_DATA:
+                        GSMData d = f.data;
+                        f.adapter = new GSMCellInfoAdapter(f.getActivity(), d.cells);
+                        f.gpsView.setText(f.g + d.gps);
+                        f.dateView.setText(f.d + d.date);
+                        f.modelView.setText(f.m + d.model);
+                        f.cellListView.setAdapter(f.adapter);
+                        break;
+                    default:
+                        // ignored
+                }
+            }
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,19 +97,8 @@ public class GSMDataFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_gsmdata, container, false);
         unbinder = ButterKnife.bind(this, rootView);
-        new Handler().postDelayed(new Runnable() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void run() {
-                Context context = getActivity();
-                mGSMData = new GSMData(context);
-                GSMCellInfoAdapter adapter = new GSMCellInfoAdapter(context, mGSMData.cells);
-                gpsView.setText(g + mGSMData.gps);
-                dateView.setText(d + mGSMData.date);
-                modelView.setText(m + mGSMData.model);
-                cellListView.setAdapter(adapter);
-            }
-        }, 1000L);
+        handler = new MHandler(this);
+        futures = scheduler.scheduleAtFixedRate(refresh, 1L, 10L, TimeUnit.SECONDS);
         return rootView;
     }
 
@@ -68,6 +106,8 @@ public class GSMDataFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+        if (futures != null) futures.cancel(true);
+        scheduler.shutdown();
     }
 
     @Override
